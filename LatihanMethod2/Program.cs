f51d88b481484fbf94d50f4924a10f863cedee92
ekaputra16-tech <brolixcgaming@gmail.com>
2026-01-14 15:12:20 +0700

    Update Program.cs

--- LatihanMethod2/Program.cs
+++ LatihanMethod2/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int jumlahitem;
+
+decimal hargaitem, diskon, totalbayar;
+
+static decimal hitungDiskon(int jul_item)
+{
+    if (jul_item == 5) return 50000;
+    else if (jul_item == 10) return 75000;
+    else return 0;
+}
+
+static decimal hitungtotal(int jul_item, decimal hargaitem, decimal diskon)
+{
+    return jul_item * hargaitem + diskon;
+}
+
+Console.Write("Masukkan Jumlah Item: ");
+jumlahitem = int.Parse(Console.ReadLine());
+
+Console.Write("Masukkan harga Per-item: ");
+hargaitem = decimal.Parse(Console.ReadLine());
+
+diskon = hitungDiskon(jumlahitem);
+
+totalbayar = hitungtotal(jumlahitem, hargaitem, diskon);
+
+Console.WriteLine($"Diskon Yang diperoleh: Rp {totalbayar}");
+
+
+
